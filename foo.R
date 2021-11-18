8466bd40b8a73b8a9adb0cedcbbd528828286e47
zzzoohny <lee138@mymail.sim.edu.sg>
2021-11-18 17:49:29 +0800

    commit/push code

---
+++ foo.R
@@ -0,0 +1,14 @@
+ #radius
+r<-2
+
+#Function to compute the volumne of a sphere with radius r
+volume<-function(r,rho){
+  3/4*pi*r^2
+}
+
+#Function to compute the volumes of the spheres with radius r,r^2,r^3
+volumn_vector<-function(r){
+  r<-22
+  for (r in 2:4){
+  }
+}
